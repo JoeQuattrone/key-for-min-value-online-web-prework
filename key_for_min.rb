c734f98b010f022104062ae603347c3d9bbdef6b
Joe Quattrone <joequattrone5@gmail.com>
2018-10-21 22:05:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
   
   #hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
-  if name_hash = nil
+  if name_hash = {}
     return nil
   else
     min_value = name_hash.first[1]
