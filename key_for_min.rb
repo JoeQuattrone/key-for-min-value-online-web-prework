217bf21b93cab621f513b9af05bc2cb312bbdf96
Joe Quattrone <joequattrone5@gmail.com>
2018-10-21 21:54:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
   #hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  min_value = name_hash[1]
-  min_key = name_hash[0]
+  min_value = name_hash.first[1]
+  min_key = name_hash.first[0]
   name_hash.each do |key, value|
     if value < min_value
       min value = value
